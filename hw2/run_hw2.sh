e8484b3fe83b9ec6ed234e56300f2932710b49d9
so-olitary <kill143@mail.ru>
2015-10-22 23:12:11 +0300

    python-sh

--- hw2/run_hw2.sh
+++ hw2/run_hw2.sh
@@ -1,10 +1,11 @@
 ﻿#!/bin/sh
 
 INPUT='/data/patents/cite75_99.txt'
-OUTPUT='PageRank_Step_0'
+OUTPUT='PageRank_python_Step_0'
 
 hadoop fs -rm -r ${OUTPUT}
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -D mapreduce.job.name=${OUTPUT} \
     -file mapper_p2.py reducer_p2.py \
     -mapper mapper_p2.py \
     -reducer reducer_p2.py \
@@ -14,16 +15,16 @@ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
 for ((i=1;i<=30;i++))
 do
     INPUT=${OUTPUT}
-    OUTPUT='PageRank_Step_'${i}
+    OUTPUT='PageRank_python_Step_'${i}
 
     hadoop fs -rm -r ${OUTPUT}
     hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+		-D mapreduce.job.name=${OUTPUT} \
         -file mapper_hw2.py reducer_hw2.py \
         -mapper mapper_hw2.py \
         -reducer reducer_hw2.py \
         -input ${INPUT} \
         -output ${OUTPUT}
     hadoop fs -rm -r ${INPUT}
-
-    hadoop fs -text ${OUTPUT}/part* | sort -k2,2nr | head > ${OUTPUT}_top.txt
 done
+
